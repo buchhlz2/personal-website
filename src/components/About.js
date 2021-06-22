e0f55c422ab327a2348a1be8a086fd83906acd62
Dan Buchholz <buchhlz2@gmail.com>
2021-06-21 21:08:38 -0700

    style: fix some alignment issues

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import BasicInfo from './BasicInfo'
-import { Icon, InlineIcon } from '@iconify/react'
+import { Icon } from '@iconify/react'
 import redCircle from '@iconify/icons-emojione/red-circle'
 import yellowCircle from '@iconify/icons-twemoji/yellow-circle'
 import greenCircle from '@iconify/icons-twemoji/green-circle'
@@ -12,16 +12,16 @@ const About = (props) => {
 				<BasicInfo />
 				<div className='col-md-8 mt-3'>
 					<div className='col-md-10 mx-auto'>
-						<div className='card' id='aboutMeCard'>
+						<div className='card' id='about-me-card'>
 							<div
-								className='card-header'
+								className='card-header d-flex'
 								style={{
 									background: '#6d6d6d',
 									color: 'white',
 									height: '2em',
 								}}
 							>
-								<div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+								<div id='about-icon-div' style={{ position: 'absolute' }}>
 									<Icon icon={redCircle} />
 									&nbsp;
 									<Icon icon={yellowCircle} />
@@ -29,46 +29,45 @@ const About = (props) => {
 									<Icon icon={greenCircle} />
 									&nbsp;
 								</div>
+								<div className='d-flex justify-content-center' style={{ width: '100%' }}>
+									~/.env
+								</div>
 							</div>
-							<div
-								className='card-body font-trebuchet text-justify ml-3 mr-3'
-								style={{
-									height: 'auto',
-
-									background: 'white',
-								}}
-							>
-								Hi, I'm Dan.
-								<br />
-								<br />
+							<div className='card-body font-trebuchet text-justify ml-3 mr-3' id='about-me-content'>
 								<p>
-									Passion for startups and all things tech &#8212; Chicago native, currently based in San Francisco.
-									Engineering alumn of the{' '}
+									<b>NAME</b>="DAN"
+								</p>
+
+								<p>
+									<b>DESCRIPTION</b>="Passion for startups and all things tech &#8212; Chicago native, currently based
+									in San Francisco. Engineering alumn of the{' '}
 									<a
 										href='https://en.wikipedia.org/wiki/Grainger_College_of_Engineering'
 										target='_blank'
 										rel='noopener noreferrer'
 									>
-										University of Illinois at Urbana-Champaign.
+										University of Illinois at Urbana-Champaign
 									</a>
+									."
 								</p>
 								<p>
-									Intrigued by all things physics, biohacking, crypto, the Universe, math, and music. Essentially, a
-									slight obsession with "the science of" why things exist as they do.
+									<b>INTERESTS</b>="Intrigued by all things physics, biohacking, crypto, the Universe, math, food
+									science, fitness, and music. Essentially, a slight obsession with "the science of" why things exist as
+									they do."
 								</p>
 								<p>
-									Looking to{' '}
+									<b>DESIRE</b>="Looking to{' '}
 									<a href='https://a16z.com/2020/04/18/its-time-to-build/' target='_blank' rel='noopener noreferrer'>
 										#build
 									</a>{' '}
-									something special.
+									something special."
 								</p>
 								<p>
-									Say hi on{' '}
+									<b>CONTACT</b>="Say hi on{' '}
 									<a href='https://www.twitter.com/dtbuchholz' target='_blank' rel='noopener noreferrer'>
 										Twitter
 									</a>{' '}
-									or any of the social platforms below.
+									or any of the social platforms below."
 								</p>
 							</div>
 						</div>
